9f3403b54440506e33d34bbcc4309333eaaf53b6
NJonathanJiang <n.jonathan.jiang@gmail.co,m>
2023-10-24 15:43:17 -0500

    berry -> candy

--- src/gameObjects.js
+++ src/gameObjects.js
@@ -12,11 +12,12 @@ class BerryObject extends GameObject {
     }
 
     draw(ctx) {
-        let dy = Math.sin(Date.now() / 1000) * 0.1;
+        let dy = Math.sin(Date.now() / 500) * 0.05;
         ctx.translate(0, dy)
         super.draw(ctx);
     }
 }
+
 
 class PlatformObject extends GameObject {
     /**
@@ -32,7 +33,7 @@ class PlatformObject extends GameObject {
 
     draw(ctx){
         ctx.fillStyle = "#4cf";
-        ctx.fillRect(-0.51, -0.51, 1.02, 1.02);
+        ctx.fillRect(-0.5, -0.5, 1.0, 1.0);
     }
 }
 
@@ -52,6 +53,7 @@ class BoxObject extends GameObject {
     }
 }
 
+
 class PumpkinObject extends GameObject {
     static image = document.getElementById('pump-img');
 
@@ -60,7 +62,7 @@ class PumpkinObject extends GameObject {
      * @param {number} y
      */
     constructor(x, y) {
-        super(new Vec2(1, 0.8), PumpkinObject.image, {isStatic: true});
+        super(new Vec2(1, 1.2), PumpkinObject.image, {isStatic: true});
         this.body.isSensor = true;
         this.position = new Vec2(x, y);
     }
@@ -72,41 +74,93 @@ class PumpkinObject extends GameObject {
     }
 }
 
+
 class DoorObject extends GameObject {
-    open = false;
+    open_progress = 0;
+    initial_height; initial_y;
+    color;
+
     /**
      * @param {number} x
      * @param {number} y
-     * @param sx
-     * @param sy
+     * @param {number} sx
+     * @param {number} sy
+     * @param {string} color
      */
-    constructor(x, y, sx=1, sy=1) {
+    constructor(x, y, sx, sy, color) {
         super(new Vec2(sx, sy), null, {isStatic: true, friction: 0.1});
-        this.position = new Vec2(x + sx/2, y + sy/2);
+        this.color = color;
+        this.position = new Vec2(x, y);
+        this.initial_height = sy;
+        this.initial_y = y;
+        /** @type {ButtonObject[]} */
+        this.linked_buttons = [];
+    }
+
+    /** @param {ButtonObject} button */
+    link(button) { this.linked_buttons.push(button); button.color = this.color }
+
+    update(engine, deltaTime) {
+        let shouldOpen = !this.linked_buttons.find(i => !i.isPressed);
+        if(shouldOpen) {
+            this.open_progress = Math.min(this.open_progress + deltaTime * 0.005 / Math.abs(this.initial_height), 1);
+        }
+        else {
+            this.open_progress = Math.max(this.open_progress - deltaTime * 0.005 / Math.abs(this.initial_height), 0);
+        }
+        if(this.open_progress === 1){
+            this.y = 1000;
+        }
+        else {
+            let h = this.initial_height * (1 - (this.open_progress));
+            let y = this.initial_y - this.initial_height / 2 + h / 2;
+
+            this.size = this.size.setY(h);
+            this.y = y;
+        }
     }
 
     draw(ctx){
-        ctx.fillStyle = "#4cf";
+        ctx.fillStyle = this.color;
         ctx.fillRect(-0.51, -0.51, 1.02, 1.02);
     }
 }
 
+
 class ButtonObject extends GameObject {
-    static image = document.getElementById('berry-img');
+    static image = document.getElementById('button-neutral-img');
+    static image_pressed = document.getElementById('button-pressed-img');
+
+    /** @type {GameEngine} */
+    engine;
 
     /**
      * @param {number} x
      * @param {number} y
+     * @param {GameEngine} engine
      */
-    constructor(x, y) {
-        super(new Vec2(0.8, 0.8), BerryObject.image, {isStatic: true});
+    constructor(x, y, engine) {
+        super(new Vec2(0.8, 0.15), ButtonObject.image_pressed, {isStatic: true});
+        this.engine = engine;
         this.position = new Vec2(x, y);
         this.collisionLayer = 31;
+        this.color = '#f00'
     }
 
     draw(ctx) {
-        let dy = Math.sin(Date.now() / 1000) * 0.1;
-        ctx.translate(0, dy)
-        super.draw(ctx);
+        if (!this.isPressed) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(-0.5, -0.5, 1, 0.5);
+        }
+        ctx.fillStyle = "#888";
+        ctx.fillRect(-0.625, 0, 1.25, 0.5);
+    }
+
+    get isPressed() {
+        return this.engine.gameObjects.find(i =>
+            i !== this &&
+                (i instanceof BoxObject || i instanceof PlayerObject || i instanceof ShadowPlayerObject)
+            && this.isCollidingWith(i)
+        );
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,14 +2,45 @@ const renderer = new Renderer(document.getElementById('game-canvas'));
 window.onresize = (f => (f(), f))(() => renderer.resize());
 
 const levels = [
-    // new Level(document.getElementById("level-1-background"), function(level, player, berry){
+    new Level(document.getElementById("level-1-background"), function(level, player, berry){}),
+    new Level(document.getElementById("level-1-background"), function(level, player, berry){
+        level.addPlatformAt(7.5, 1, 1, 5);
 
-    // }),
-    // new Level(document.getElementById("level-1-background"), function(level, player, berry){
-    //     level.addBoxAt(5.5, 7.5);
-    //     level.addBoxAt(7.5, 7.5);
-    //     level.addBoxAt(9.5, 7.5);
-    // }),
+        let buttonObject = new ButtonObject(1.5, 8, level.engine);
+        level.engine.add(buttonObject);
+        let doorObject = new DoorObject(8, 7, 1, 2, "#fac");
+        level.engine.add(doorObject);
+        doorObject.link(buttonObject);
+
+        level.addPlatformAt(3, 6.25, 2, .5);
+        level.addBoxAt(4, 5.75);
+    }),
+    new Level(document.getElementById("level-1-background"), function(level, player, berry){
+        level.addPlatformAt(7.5, 1, 1, 4);
+
+        let buttonObject = new ButtonObject(1.5, 8, level.engine);
+        level.engine.add(buttonObject);
+        let buttonObject2 = new ButtonObject(6, 8, level.engine);
+        level.engine.add(buttonObject2);
+        let doorObject = new DoorObject(8, 7.5, 1, 1, "#fac");
+        level.engine.add(doorObject);
+        doorObject.link(buttonObject);
+        doorObject.link(buttonObject2);
+
+        level.addPlatformAt(7.5, 6, 1, 1);
+
+        let buttonObject3 = new ButtonObject(14.5, 8, level.engine);
+        level.engine.add(buttonObject3);
+        let doorObject2 = new DoorObject(8, 5.5, 1, 1, "#8fa");
+        level.engine.add(doorObject2);
+        doorObject2.link(buttonObject3);
+
+        level.addPlatformAt(3, 6.25, 2, .5);
+        level.addBoxAt(4, 5.75);
+        level.addBoxAt(4, 4.75);
+
+        berry.y -= 2;
+    }),
     new Level(document.getElementById("level-1-background"), function(level, player, berry){
         level.addPlatformAt(9, 4.5, 1, 5);
 
@@ -24,19 +55,19 @@ const levels = [
         level.addPlatformAt(4, 6.25, 2, .5);
         level.addBoxAt(4, 5.75);
     }, function(level) {
-        level.addPumpkinAt(1.5, 5.5);
+        level.addPumpkinAt(5, 4);
     }),
-    new Level(document.getElementById("level-1-background"), function(level, player, berry){
-        level.addPlatformAt(4, 1, 1, 1);
-        level.addPlatformAt(2, 3, 1, 1);
-
-        level.addBoxAt(new BoxObject(2.5, 2));
-
-        player.position = new Vec2(1.5, 1.5);
-        berry.position = new Vec2(4.5, 7.5);
-    }, function(level) {
-        level.addPumpkinAt(2.5, 7);
-    }),
+    // new Level(document.getElementById("level-1-background"), function(level, player, berry){
+    //     level.addPlatformAt(4, 1, 1, 1);
+    //     level.addPlatformAt(2, 3, 1, 1);
+    //
+    //     level.addBoxAt(new BoxObject(2.5, 2));
+    //
+    //     player.position = new Vec2(1.5, 1.5);
+    //     berry.position = new Vec2(4.5, 7.5);
+    // }, function(level) {
+    //     level.addPumpkinAt(2.5, 7);
+    // }),
 ]
 
 let i = 0;
