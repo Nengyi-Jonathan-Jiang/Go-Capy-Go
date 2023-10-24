b2938c1c6cf3baa4cdbd2c9fa82917d7cef3899c
K1334755 <theNerdyMango@gmail.com>
2023-10-24 09:06:13 -0500

    .

--- src/gameObjects.js
+++ src/gameObjects.js
@@ -6,7 +6,7 @@ class BerryObject extends GameObject {
      * @param {number} y
      */
     constructor(x, y) {
-        super(new Vec2(0.65, 0.7), BerryObject.image, {isStatic: true});
+        super(new Vec2(0.8, 0.8), BerryObject.image, {isStatic: true});
         this.position = new Vec2(x, y);
         this.collisionLayer = 31;
     }
@@ -69,5 +69,44 @@ class PumpkinObject extends GameObject {
         ctx.globalAlpha = Math.sin(Date.now() / 200) * 0.2 + 0.6;
         super.draw(ctx);
         ctx.globalAlpha = 1;
+    }
+}
+
+class DoorObject extends GameObject {
+    open = false;
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param sx
+     * @param sy
+     */
+    constructor(x, y, sx=1, sy=1) {
+        super(new Vec2(sx, sy), null, {isStatic: true, friction: 0.1});
+        this.position = new Vec2(x + sx/2, y + sy/2);
+    }
+
+    draw(ctx){
+        ctx.fillStyle = "#4cf";
+        ctx.fillRect(-0.51, -0.51, 1.02, 1.02);
+    }
+}
+
+class ButtonObject extends GameObject {
+    static image = document.getElementById('berry-img');
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
+    constructor(x, y) {
+        super(new Vec2(0.8, 0.8), BerryObject.image, {isStatic: true});
+        this.position = new Vec2(x, y);
+        this.collisionLayer = 31;
+    }
+
+    draw(ctx) {
+        let dy = Math.sin(Date.now() / 1000) * 0.1;
+        ctx.translate(0, dy)
+        super.draw(ctx);
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ const renderer = new Renderer(document.getElementById('game-canvas'));
 window.onresize = (f => (f(), f))(() => renderer.resize());
 
 const levels = [
-    new Level(document.getElementById("level-1-background"), function(level, player, berry){
+    // new Level(document.getElementById("level-1-background"), function(level, player, berry){
 
-    }),
-    new Level(document.getElementById("level-1-background"), function(level, player, berry){
-        level.addBoxAt(5.5, 7.5);
-        level.addBoxAt(7.5, 7.5);
-        level.addBoxAt(9.5, 7.5);
-    }),
+    // }),
+    // new Level(document.getElementById("level-1-background"), function(level, player, berry){
+    //     level.addBoxAt(5.5, 7.5);
+    //     level.addBoxAt(7.5, 7.5);
+    //     level.addBoxAt(9.5, 7.5);
+    // }),
     new Level(document.getElementById("level-1-background"), function(level, player, berry){
         level.addPlatformAt(9, 4.5, 1, 5);
 
         level.addPlatformAt(4, 6.25, 2, .5);
         level.addBoxAt(4, 5.75);
     }, function(level) {
-        level.addPumpkinAt(1.5, 5.5);
+        level.addPumpkinAt(5, 4);
     }),
     new Level(document.getElementById("level-1-background"), function(level, player, berry){
         level.addPlatformAt(9, 3.5, 1, 7);
